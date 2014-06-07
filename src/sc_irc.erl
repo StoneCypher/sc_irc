f1fcc4e450a03c73148a6e427bdfc89fd19c48c9
John Haugeland <stonecypher@gmail.com>
2014-06-07 00:54:04 -0700

    scratch

---
+++ src/sc_irc.erl
@@ -0,0 +1,33 @@
+
+-module(sc_irc).
+
+
+
+
+
+-export( [
+
+	connect/1,
+	disconnect/1
+
+] ).
+
+
+
+
+
+%% @doc Connect to a server.
+
+connect(_Config) ->
+
+    todo.
+
+
+
+
+
+%% @doc Disconnect from a server.
+
+disconnect(_Pid) ->
+
+    todo.
