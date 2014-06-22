9901d109c245b4da6b51efee72ca85145e88039b
John Haugeland <stonecypher@gmail.com>
2014-06-21 23:18:34 -0700

    util module begin; move irc_strcmp_i and _nordfix

--- src/sc_irc.erl
+++ src/sc_irc.erl
@@ -28,9 +28,6 @@
 	connect/1,
 	disconnect/1,
 
-    irc_strcomp_i/2,
-      irc_strcomp_i_nordfix/1,
-
 	channel_type/1,
 	  legal_channel_name_char/1,
 
@@ -134,33 +131,3 @@ parse_message(Message) when is_list(Message) ->
 
     [Command | Parameters] = sc:explode(" ", Message),
     #irc_message{ command=Command, parameters=Parameters, paramstring=sc:implode(" ", Parameters) }.
-
-
-
-
-
-%% @doc Destructively prepares a string for comparison in the IRC nordic characterset.  Throws away substantial information and returns the string reversed.
-
-irc_strcomp_i_nordfix(String) ->
-
-    irc_strcomp_i_nordfix(String, []).
-
-
-
-irc_strcomp_i_nordfix([],         Work)                         -> Work;
-
-irc_strcomp_i_nordfix([${ | Rem], Work)                         -> irc_strcomp_i_nordfix(Rem, [$[]             ++ Work);  % section 2.2, character codes
-irc_strcomp_i_nordfix([$} | Rem], Work)                         -> irc_strcomp_i_nordfix(Rem, [$]]             ++ Work);  % {}| are upper case []\
-irc_strcomp_i_nordfix([$| | Rem], Work)                         -> irc_strcomp_i_nordfix(Rem, [$\\]            ++ Work);  % http://irchelp.org/irchelp/rfc/chapter2.html
-irc_strcomp_i_nordfix([Ch | Rem], Work) when Ch >= $a, Ch =< $z -> irc_strcomp_i_nordfix(Rem, [Ch - ($a - $A)] ++ Work);
-irc_strcomp_i_nordfix([Ex | Rem], Work)                         -> irc_strcomp_i_nordfix(Rem, [Ex]             ++ Work).
-
-
-
-
-
-%% @doc Case insensitively compares two strings given IRC's definition that `{}|' constitute lower-case `[]\\'.
-
-irc_strcomp_i(Str1, Str2) ->
-
-    string:equal( irc_strcomp_i_nordfix(Str1), irc_strcomp_i_nordfix(Str2) ).
