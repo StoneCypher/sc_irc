56d020e6353f4f4aeb1979ec1f0927704a62b799
John Haugeland <stonecypher@gmail.com>
2014-06-21 23:39:59 -0700

    cmd:nick/1

--- src/sc_irc_cmd.erl
+++ src/sc_irc_cmd.erl
@@ -21,4 +21,16 @@ pass(Password) when is_list(Password) ->
 
     "PASS " ++ Passwd.
 
-    % expect ERR_NEEDMOREPARAMS or ERR_ALREADYREGISTRED
+    % expect ERR_NEEDMOREPARAMS | ERR_ALREADYREGISTRED
+
+
+
+
+
+nick(NewNick) when is_list(Nick) ->
+
+    sc_irc_util:throw_on_illegal_param(Nick),
+
+    "NICK " ++ Passwd.
+
+    % expect ERR_NONICKNAMEGIVEN | ERR_ERRONEUSNICKNAME | ERR_NICKNAMEINUSE | ERR_NICKCOLLISION
